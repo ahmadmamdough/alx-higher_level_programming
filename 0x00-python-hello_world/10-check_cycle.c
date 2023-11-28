cdb90918283be25d1cfff730fafc12f076348222
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-11-28 02:43:33 +0200

    hotfix

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,17 @@
+/**
+ * check_cycle - checks for cycle
+ * @list: head
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *it = list;
+
+	while (it->next)
+	{
+		it = it->next;
+		if (it == list)
+			return (1);
+	}
+	return (0);
+}
