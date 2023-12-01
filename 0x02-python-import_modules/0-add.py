16f843055c4470c93dc87ec416bafaa0a2e9a526
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-12-01 03:48:55 +0200

    hotfix

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# this is import
 if __name__ == "__main__":
-	import add_0
-	a = 1
-	b = 2
-	print(f"{a} + {b} = {add_0.add(a,b)}")
+    import add_0 
+
+    a = 1
+    b = 2
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
