1adebe79221cd03e4bcbfc78195a740ab3b84bac
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-12-05 01:50:25 +0200

    wow

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 def no_c(my_string):
     new_str = my_string
     new_str = my_string.replace('c', '')
-    new_str = new_str.repalce('C', '')
+    new_str = new_str.replace('C', '')
     return (new_str)
