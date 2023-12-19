f567fa1727964aec47a6fe281c661c1675c14ee1
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-12-19 02:45:56 +0200

    w

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(mylist[i]), end='')
+            n += 1
+        except (TypeError, ValueError)
+            continue
+        except IndexError:
+            break
+    print('')
+    return n
