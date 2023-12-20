21f99bc6f4cff5617c66d31151a027bfc08e6b03
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-12-20 04:39:21 +0200

    w

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,7 +6,7 @@
 class Square:
     """This is a square representaion
     """
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
