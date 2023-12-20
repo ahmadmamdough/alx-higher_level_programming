d33b5bf35bbb86ec6fc8e93f2737996b3e58f891
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-12-20 04:27:01 +0200

    w

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,8 +7,8 @@ class Square:
     """This is a square representaion
     """
     def __init__(self, size=0):
-            self.size = size
-   
+        self.size = size
+
     @property
     def size(self):
         return self.__size
@@ -21,6 +21,6 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-    
+
     def area(self):
         return self.__size ** 2
