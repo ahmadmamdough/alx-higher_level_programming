9ab9271d0ab9d08b7afcecd9b7c5eb0aa0306089
ahmadmamdough <ahmedmmdoh404@gmail.com>
2023-12-20 04:00:11 +0200

    w

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""This is squares vol. 1
+"""
+
+
+class Square:
+    """This is a square representaion
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0") 
+        else:
+            self.__size = size 
